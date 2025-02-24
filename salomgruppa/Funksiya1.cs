81cf77e6457e4ce5307d1d4757b61c335c0e152c
dilmurod-jabborov <futboluz825@gmail.com>
2025-02-24 17:57:56 +0500

    tamom

---
+++ salomgruppa/Funksiya1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salomgruppa
+{
+    internal class Funksiya1
+    {
+        Console.WriteLine("Hello, World!"); 
+    }
+}
